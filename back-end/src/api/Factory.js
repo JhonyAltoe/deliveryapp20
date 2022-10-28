19e373e8f6dd14bd120cca32ca99a2ff9add1f47
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-10-28 11:09:15 -0300

    feat: adiciona o repository SaleProduct

--- back-end/src/api/Factory.js
+++ back-end/src/api/Factory.js
@@ -1,15 +1,15 @@
 const model = require('../database/models');
 
 const UserRepository = require('./repository/User');
-// const ProductRepository = require('./repository/Product');
+const ProductRepository = require('./repository/Product');
 // const SaleRepository = require('./repository/Sale');
-// const SaleProductRepository = require('./repository/SaleProduct');
+const SaleProductRepository = require('./repository/SaleProduct');
 
 const UserService = require('./services/User');
 const UserController = require('./controllers/User');
 
-// const ProductService = require('./services/Product');
-// const ProductController = require('./controllers/Product');
+const ProductService = require('./services/Product');
+const ProductController = require('./controllers/Product');
 
 // const SaleService = require('./services/Sale');
 // const SaleController = require('./controllers/Sale');
@@ -36,12 +36,12 @@ class Factory {
   //   };
   // }
 
-  // static product() {
-  //   const productRepository = new ProductRepository(model.Product);
-  //   const productService = new ProductService(productRepository);
-  //   const productController = new ProductController(productService);
-  //   return { productController, productService, productRepository };
-  // }
+  static product() {
+    const productRepository = new ProductRepository(model.Product);
+    const productService = new ProductService(productRepository);
+    const productController = new ProductController(productService);
+    return { productController, productService, productRepository };
+  }
 }
 
 module.exports = Factory;
--- back-end/src/api/routes/router.js
+++ back-end/src/api/routes/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const productRouter = require('./productRouter');
+const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 const imageRouter = require('./imageRouter');
 // const saleRouter = require('./saleRouter');
@@ -9,6 +9,6 @@ const route = express.Router();
 route.use(userRouter);
 // route.use(saleRouter);
 route.use('/images', imageRouter);
-// route.use(productRouter);
+route.use(productRouter);
 
 module.exports = route;
