9ea6f25efc05f5ed3ccd8e519888335603af2bb5
Nathan Kimura <nathankimura14@gmail.com>
2022-10-28 18:24:02 -0300

    fix: tranforma app em funcional e utiliza browserRouter na index

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Checkout from './pages/Checkout';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Switch>
-          <Route exact path="/" component={ Login } />
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Routes>
+      <Route path="customer/products" element={ <Checkout /> } />
+      <Route exact path="/" element={ <Navigate to="/Login" /> } />
+    </Routes>
+  );
 }
 
 export default App;
