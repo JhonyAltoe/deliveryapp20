a9761763bfc7e6b0b959bf80e81b00081e904f60
DevPBDias <devpbdias@gmail.com>
2022-10-28 16:46:57 -0300

    feat: estrutura de requisiçao da api

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import LogoGroup from '../components/LogoGroup';
 import Loading from '../components/Loading';
 import FormsLogin from '../components/FormsLogin';
+// import { requestData } from '../services/requests';
 
+// usar funcional no lugar de classe e context hooks
 class Login extends Component {
   constructor() {
     super();
@@ -11,12 +13,19 @@ class Login extends Component {
     };
   }
 
+  // usar setLeaderBoard para setar o estado de um contexto
+
+  // const getLeaderboard = (endpoint) => requestData(endpoint)
+  // .then((response) => setLeaderboard(response))
+  // .catch((error) => console.log(error));
+
   render() {
     const { fetching } = this.state;
     return (
       fetching ? <Loading />
         : (
           <div className="App">
+            <p>{api}</p>
             <LogoGroup />
             <FormsLogin />
           </div>
