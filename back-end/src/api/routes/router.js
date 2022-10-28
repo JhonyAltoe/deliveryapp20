a661ede67e66176ee7e3a96bca10c5e75dbe8d8e
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-10-28 11:14:13 -0300

    feat: adiciona a rota sales

--- back-end/src/api/routes/router.js
+++ back-end/src/api/routes/router.js
@@ -2,12 +2,12 @@ const express = require('express');
 const productRouter = require('./productRouter');
 const userRouter = require('./userRouter');
 const imageRouter = require('./imageRouter');
-// const saleRouter = require('./saleRouter');
+const saleRouter = require('./saleRouter');
 
 const route = express.Router();
 
 route.use(userRouter);
-// route.use(saleRouter);
+route.use(saleRouter);
 route.use('/images', imageRouter);
 route.use(productRouter);
 
