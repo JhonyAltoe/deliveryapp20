99bdf585b2607aaa1b28d856760615667af8ed16
felipesty <felipestylex08@gmail.com>
2022-10-31 16:46:44 -0300

    tela de registro completa

--- front-end/src/components/FormRegister.js
+++ front-end/src/components/FormRegister.js
@@ -1,7 +1,48 @@
-import React from 'react';
-// import { useHistory } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { setToken, requestRegister } from '../services/requests';
 
 function FormRegister() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [failedTryLogin, setFailedTryLogin] = useState(false);
+  const history = useHistory();
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { token, role } = await requestRegister(
+        '/register',
+        { name, email, password },
+      );
+      setToken(token);
+
+      localStorage.setItem('token', token);
+      localStorage.setItem('role', role);
+
+      history.push('/customer/products');
+    } catch (error) {
+      setFailedTryLogin(true);
+      setErrorMsg('Não foi possível efetuar o cadastro');
+    }
+  };
+
+  const handleBtn = () => {
+    const twelve = 12;
+    const nameValid = name.length >= twelve;
+
+    const regexEmail = /^\S+@\S+\.\S+$/;
+    const emailValid = regexEmail.test(email);
+
+    const five = 5;
+    const pwdValid = password.length > five;
+
+    return emailValid && pwdValid && nameValid;
+  };
+
   return (
     <form>
       <h2>Cadastro</h2>
@@ -11,6 +52,7 @@ function FormRegister() {
           type="text"
           name="name"
           id="name"
+          onChange={ ({ target: { value } }) => setName(value) }
           placeholder="Seu nome"
           data-testid="common_register__input-name"
         />
@@ -23,6 +65,7 @@ function FormRegister() {
           type="email"
           name="email"
           id="email"
+          onChange={ ({ target: { value } }) => setEmail(value) }
           placeholder="seu-email@site.com.br"
           data-testid="common_register__input-email"
         />
@@ -35,6 +78,7 @@ function FormRegister() {
           type="password"
           name="password"
           id="password"
+          onChange={ ({ target: { value } }) => setPassword(value) }
           placeholder="******"
           data-testid="common_register__input-password"
         />
@@ -44,9 +88,16 @@ function FormRegister() {
       <button
         type="button"
         data-testid="common_register__button-register"
+        disabled={ !handleBtn() }
+        onClick={ (event) => handleClick(event) }
       >
         Cadastro
       </button>
+      {
+        failedTryLogin ? (
+          <p data-testid="common_register__element-invalid_register">{ errorMsg }</p>
+        ) : ''
+      }
     </form>
   );
 }
