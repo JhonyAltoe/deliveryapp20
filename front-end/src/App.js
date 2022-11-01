9a3d759c293cd2fde8e71ccaad2f73a32f7b46c2
DevPBDias <devpbdias@gmail.com>
2022-11-01 16:45:03 -0300

    fix: removendo componente User

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import ClientOrders from './pages/ClientOrders';
 import Login from './pages/Login';
-import User from './pages/User';
 import Register from './pages/Register';
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
         render={ () => (entrance ? <Redirect to="/login" /> : null) }
       />
       <Route exact path="/login" component={ Login } />
-      <Route exact path="/customer/products" component={ User } />
       <Route exact path="/customer/orders" component={ ClientOrders } />
       <Route exact path="/register" component={ Register } />
     </Switch>
