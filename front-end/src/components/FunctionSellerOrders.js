0a048bd50771a4fb55c3e135c3f3071bc2109076
felipesty <felipestylex08@gmail.com>
2022-11-09 01:14:50 -0300

    tela OrderDetails

---
+++ front-end/src/components/FunctionSellerOrders.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function FunctionSellerOrder() {
+  return (
+    <div>
+      {a.map((a, index) => (
+        <p
+          key={ index }
+        >
+          a
+        </p>
+      ))}
+    </div>
+  );
+}
+
+export default FunctionSellerOrder;
